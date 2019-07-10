4d3a8406baf841a2cb71904088406218d320221d
Reagan Henke <reaganhenke@Reagans-MBP.lan>
2019-07-09 22:12:36 -0400

    Updates

---
+++ precache-manifest.54c334928c66b63f04268f53f7efdd15.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b0142243cd1f2f4fecc790e188e834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53ec528ef0abe9bc3d79",
+    "url": "/static/css/main.4300531f.chunk.css"
+  },
+  {
+    "revision": "12f8592e34c110f9c7fa",
+    "url": "/static/js/2.af9372b3.chunk.js"
+  },
+  {
+    "revision": "53ec528ef0abe9bc3d79",
+    "url": "/static/js/main.ed5eea68.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fd10d3158f752699a6cc44f88f424c95",
+    "url": "/static/media/8bitclips.fd10d315.mp4"
+  },
+  {
+    "revision": "f28134afd99189a722fae5eec5057da5",
+    "url": "/static/media/codepen.f28134af.png"
+  },
+  {
+    "revision": "d224b9914577c9cd0c0131bd76662514",
+    "url": "/static/media/knitclips.d224b991.mp4"
+  },
+  {
+    "revision": "242fafd40340fbf5f8f56e2229585d98",
+    "url": "/static/media/ministryoftruth.242fafd4.png"
+  },
+  {
+    "revision": "53d1c56d6bf615f3efb9526d5b9029b5",
+    "url": "/static/media/musicclips.53d1c56d.mp4"
+  },
+  {
+    "revision": "aba968a01ef9b510ddc69a6f77bcec9b",
+    "url": "/static/media/spambot_demo.aba968a0.png"
+  }
+]);
